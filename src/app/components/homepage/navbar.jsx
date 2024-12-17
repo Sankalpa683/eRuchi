f050730e38720bfcb91395f7d3a9b0990d36ab0b
Sankalpa683 <sank.dahal@gmail.com>
2024-12-17 13:48:50 +0545

    shop section accomplished

--- src/app/components/homepage/navbar.jsx
+++ src/app/components/homepage/navbar.jsx
@@ -9,18 +9,18 @@ const navbar = () => {
   return (
     <nav className='border z-50 p-3 top-0 bg-white sticky'>
       <div className='flex flex-wrap justify-between mx-4 items-center'>
-        <div>
+        <div className='cursor-pointer'>
           <Link href='/'>
-            <Image src="/logo.png" width={135} height={60} alt='eruchi_icon' />
+            <Image src="/logo.png" width={135} height={60} alt='eruchi_icon'/>
           </Link>
         </div>
-        <div className='hidden lg:flex md:flex gap-8'>
-          <Link href='/shop'> <span className='uppercase'>Shop</span></Link>
-          <Link href='/'> <span className='uppercase'>Blog</span></Link>
-          <Link href='/'> <span className='uppercase'>Survey</span></Link>
-          <Link href='/faqs'> <span>FAQs</span></Link>
+        <div className='hidden lg:flex md:flex gap-8 cursor-pointer'>
+          <Link href='/shop' className='cursor-pointer'> <span className='uppercase'>Shop</span></Link>
+          <Link href='/' className='cursor-pointer'> <span className='uppercase'>Blog</span></Link>
+          <Link href='/' className='cursor-pointer'> <span className='uppercase'>Survey</span></Link>
+          <Link href='/faqs' className='cursor-pointer'> <span>FAQs</span></Link>
         </div>
-        <div className='hidden lg:flex'>
+        <div className='hidden lg:flex cursor-pointer'>
           <Link href='/login'> <span className='uppercase'>Login</span></Link>
         </div>
 
